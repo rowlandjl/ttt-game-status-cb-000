73b15615fc779001f5e4637e545fe24548b3f203
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-23 15:26:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |combo|
     combo.each do |index|
       if position_taken?(board, index)
-        combo
+        return WIN_COMBINATIONS[index]
       end
     end
   end
